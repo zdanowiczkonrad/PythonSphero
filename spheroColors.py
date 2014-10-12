5dd1eaae68f8c9395addde217c1a8531598f4f29
Konrad Zdanowicz <zdanowicz.konrad@gmail.com>
2014-10-12 19:41:54 +0200

    Update spheroColors.py

--- spheroColors.py
+++ spheroColors.py
@@ -5,8 +5,8 @@ s = core.Sphero("/dev/tty.Sphero-OOB-AMP-SPP")
 print "Connecting to Sphero..."
 s.connect()
 print "My current color is: " , s.get_rgb().body
-iterations=3
-while iterations>0:
+iterations = 3
+while iterations > 0:
 	print "Setting color to red"
 	s.set_rgb(255,0,0)
 	print "Setting color to green"
@@ -14,7 +14,7 @@ while iterations>0:
 	print "Setting color to blue"
 	s.set_rgb(0,0,255)
 	time.sleep(0.1)
-	iterations-=1
+	iterations -= 1
 
 print "The end. Sphero goes to bed."
 s.sleep()
