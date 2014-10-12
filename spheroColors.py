6b0d6a0c289f2c1439ded4a149a9577be477a7a0
Konrad Zdanowicz <zdanowicz.konrad@gmail.com>
2014-10-12 19:41:36 +0200

    Update spheroColors.py

--- spheroColors.py
+++ spheroColors.py
@@ -1,20 +1,20 @@
-from sphero import core 	# this line imports Sphero controller
-import time					# this lines imports time utils
+from sphero import core 	
+import time			
 
-s = core.Sphero("/dev/tty.Sphero-OOB-AMP-SPP") #create Sphero controller
+s = core.Sphero("/dev/tty.Sphero-OOB-AMP-SPP")
 print "Connecting to Sphero..."
-s.connect()	#initialize connection
-print "My current color is: ",s.get_rgb().body
-i=3
-while i>0:
+s.connect()
+print "My current color is: " , s.get_rgb().body
+iterations=3
+while iterations>0:
 	print "Setting color to red"
 	s.set_rgb(255,0,0)
 	print "Setting color to green"
 	s.set_rgb(0,255,0)
 	print "Setting color to blue"
 	s.set_rgb(0,0,255)
-	time.sleep(0.1) #sleep 1 seconds
-	i-=1
+	time.sleep(0.1)
+	iterations-=1
 
 print "The end. Sphero goes to bed."
 s.sleep()
